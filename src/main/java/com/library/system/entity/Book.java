5161830d746283381faf814eecd0853217714a10
Yaroslav Horpenko <j.hprpenko@technikum.wsi.edu.pl>
2026-06-13 16:57:36 +0200

    Add Book entity class

--- src/main/java/com/library/system/entity/Book.java
+++ src/main/java/com/library/system/entity/Book.java
@@ -4,6 +4,27 @@ import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+
+@Entity
+@Table(name = "books")
+@Data
+@NoArgsConstructor
 public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String author;
+    private String isbn;
+    private int availableCopies;
+
+    public Book(String title, String author, String isbn, int availableCopies) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+        this.availableCopies = availableCopies;
+    }
+
 }
 
