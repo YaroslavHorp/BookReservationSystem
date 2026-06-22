0fdce9adf161d9f9f8fb16a17144e6b7a2e218aa
Yaroslav Horpenko <j.hprpenko@technikum.wsi.edu.pl>
2026-06-22 10:09:52 +0200

    Added service

--- src/main/java/com/library/system/service/BookService.java
+++ src/main/java/com/library/system/service/BookService.java
@@ -1,4 +1,25 @@
 package com.library.system.service;
 
+import com.library.system.entity.Book;
+import com.library.system.repository.BookRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class BookService {
+    private final BookRepository bookRepository;
+
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public List<Book> getAllBooks() {
+        return bookRepository.findAll();
+    }
+
+    public Book getBookById(Long id) {
+        return bookRepository.findById(id)
+                        .orElseThrow(() -> new RuntimeException("Didn't found book with id: " + id));
+    }
 }
